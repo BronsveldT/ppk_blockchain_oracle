5747236cfa138751cb5c220b9d481185d37bf7e4
Thomas Bronsveld <Thomasbronsveld94@gmail.com>
2023-10-13 21:51:10 +0200

    Initial commit

---
+++ src/main/java/com/capgemini/PPK_Blockchain_oracle/PpkBlockchainOracleApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.PPK_Blockchain_oracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpkBlockchainOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpkBlockchainOracleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/PPK_Blockchain_oracle/PpkBlockchainOracleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.PPK_Blockchain_oracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpkBlockchainOracleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
